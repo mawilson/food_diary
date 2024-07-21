99d83e869c4045a1c872537d5eee8f53d641c28c
Matt Wilson <mawilson2012@gmail.com>
2024-07-20 21:39:52 -0700

    Spring initializer boilerplate

---
+++ src/test/java/com/foodDiary/foodDiary/FoodDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodDiary.foodDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
