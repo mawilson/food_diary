ba206f993a1c25823490fb7fff57efb0a4d6b640
Matt Wilson <mawilson2012@gmail.com>
2024-07-26 12:15:17 -0700

    Added a test Added a test Added Thymeleaf templating Added a model to index Added spring devtools

--- src/test/java/com/foodDiary/foodDiary/FoodDiaryApplicationTests.java
+++ src/test/java/com/foodDiary/foodDiary/FoodDiaryApplicationTests.java
@@ -1,13 +1,28 @@
 package com.foodDiary.foodDiary;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FoodDiaryApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void getIndex() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
 	}
 
 }
